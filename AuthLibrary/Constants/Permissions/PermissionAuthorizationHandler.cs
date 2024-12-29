548b252a28fc34a1a4407e21f2b7065da5c3583f
georg <devnetdot@proton.me>
2024-12-29 16:36:20 +0300

    009 Added PermissionAuthorizationHandler

---
+++ AuthLibrary/Constants/Permissions/PermissionAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthLibrary.Constants.Permissions
+{
+    public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        public PermissionAuthorizationHandler()
+        {
+            
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            if(context.User is null)
+            {
+                await Task.CompletedTask;
+            }
+
+            var permissions = context.User.Claims
+                .Where(c => c.Type == AppClaim.Permission
+                    && c.Value == requirement.Permission
+                    && c.Value == AppClaim.Issuer);
+            if(permissions.Any())
+            {
+                context.Succeed(requirement);
+                await Task.CompletedTask;
+            }
+        }
+    }
+}
