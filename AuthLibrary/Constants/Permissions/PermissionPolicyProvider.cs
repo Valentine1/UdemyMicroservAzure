f6789752974aef3f0a1391790b476f2ab8597472
georg <devnetdot@proton.me>
2024-12-29 14:50:20 +0300

    007-008 Added Authorization package dep, added PermissionPolicyProvider

---
+++ AuthLibrary/Constants/Permissions/PermissionPolicyProvider.cs
@@ -0,0 +1,36 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace AuthLibrary.Constants.Permissions
+{
+    public class PermissionPolicyProvider : IAuthorizationPolicyProvider
+    {
+        public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+        public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+        {
+            FallbackPolicyProvider = new (options);
+        }
+
+        public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+        {
+            return FallbackPolicyProvider.GetDefaultPolicyAsync();
+        }
+
+        public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+        {
+            return Task.FromResult<AuthorizationPolicy>(null);
+        }
+
+        public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            if(policyName.StartsWith(AppClaim.Permission, StringComparison.CurrentCultureIgnoreCase))
+            {
+                var policy = new AuthorizationPolicyBuilder();
+                policy.AddRequirements(new PermissionRequirement(policyName));
+                return Task.FromResult(policy.Build());
+            }
+            return FallbackPolicyProvider.GetPolicyAsync(policyName);
+        }
+    }
+}
