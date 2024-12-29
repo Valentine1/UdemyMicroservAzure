a4c04fe551237f1ab4f2d5f02e7b1d87660d2fc0
georg <devnetdot@proton.me>
2024-12-29 14:48:41 +0300

    006 Added app claims constants

---
+++ AuthLibrary/Constants/Authentication/AppClaim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthLibrary.Constants.Authentication
+{
+    public static class AppClaim
+    {
+        public const string Permission = "permission";
+        public const string Expiration = "exp";
+        public const string Issuer = "gateway";
+    }
+}
